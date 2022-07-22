f17adfe38c25dd51e35327a820f6ce355fdb7567
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-07-22 23:57:00 +0700

    Update 2022_07_22_143447_create_posts_table.php

--- annas-blog/database/migrations/2022_07_22_143447_create_posts_table.php
+++ annas-blog/database/migrations/2022_07_22_143447_create_posts_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->text('body');
             $table->timestamp('publish_at')->nullable();
             $table->timestamps();
-            
         });
     }
 
