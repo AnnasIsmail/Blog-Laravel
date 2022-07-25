d3353efbec0d98ca0a670b24ca91883016d6548c
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-07-25 16:19:58 +0700

    Update category.blade.php

--- annas-blog/resources/views/category.blade.php
+++ annas-blog/resources/views/category.blade.php
@@ -4,12 +4,13 @@
 @section('container')
 
     @foreach ($posts as $post)
-    <article class="mb-4">
+    <article class="mb-4 border-bottom pt-10">
         <h2>
             <a class="text-decoration-none" href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
         </h2>
         <h5>{{ $post->author }}</h5>
         <p>{{ $post->excerpt }}</p>
+        <a href="/posts/{{ $post->slug }}">Read more...</a>
     </article>
     @endforeach
 
